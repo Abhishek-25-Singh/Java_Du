211b5f954553f822d05e41ca104bce27e058de9e
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-15 23:40:17 +0530

    Infinity and NaN

--- Operators/Operators1.java
+++ Operators/Operators1.java
@@ -73,6 +73,29 @@ System.out.println("pre-increment: "+ y);
 
 // double c = 0.89;
 // System.out.println(a+c); //?---> char + Double = Double
+
+//*---------------INfinity------------------------- */
+
+//!Runtime Exception: ArithmeticException: / by zero
+// System.out.println(10/0);
+ //?----> Infinity
+// System.out.println(10/0.0); 
+//?----> -Infinity 
+// System.out.println(-10/0.0);   
+
+//*---------------NaN(Not a Number)------------------------- */
+// System.out.println(0.0/0);
+// System.out.println(-0.0/0);
+
+
+//*---------------float.NaN------------------------- */
+// System.out.println(10<Float.NaN);
+// System.out.println(10<=Float.NaN);
+// System.out.println(10>Float.NaN);
+// System.out.println(10>=Float.NaN);
+// System.out.println(10==Float.NaN);
+// System.out.println(10!=Float.NaN);
+// System.out.println(10!=Float.NaN);
     }
 }
 
