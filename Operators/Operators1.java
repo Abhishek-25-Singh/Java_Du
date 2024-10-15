0f6c2dbaf9e894cccfcf6b2fcd873ee8a326c65a
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-15 22:16:38 +0530

    Increment and Decrement Operator
                      👉Increment and Decrement Operator👈
    🎯 Increment
         🔸  pre-increment
         🔸  post-increment
    ✦ pre-increment -  Increment Then Assign Value ( ++x )
    ✦ pre-increment -  Assign Value Then Increment  ( x ++)
    
    🎯 decrement
         🔸  pre-decrement
         🔸  post-decrement
    ✦ pre-decrement -  decrement Then Assign Value ( --x )
    ✦ pre-decrement -  Assign Value Then decrement ( x --)

--- Operators/Operators1.java
+++ Operators/Operators1.java
@@ -1,11 +1,18 @@
 public class Operator1 {
     public static void main(String[] args){
 
+//*--------------------------Increment And Decrement-----------------------
+// int x = 10;
+// int y = 10;
 
+// System.out.println("pre-increment: "+ (++x));
+// System.out.println("Value of x :"+ x);
 
-
-
-
+// System.out.println("post-increment: "+ (y++));
+// System.out.println("Value of x :"+ y);
+int x = 10;
+int y = ++x;
+System.out.println("pre-increment: "+ y);
 
 
 
