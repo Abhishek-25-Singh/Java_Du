2dc0777ccf16fbdaa1bc99f539dfe91b2c5ce32d
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-19 23:51:10 +0530

    Switch Statement
                         👉Importance of Break Statement 👈
    🔹Var1 = 10 hain to loop ke aandar enter ho jayenge.
    🔹Kyunki case 10 and var1 = 10 ; bhi 10 hain toh  hello print 
          hoga.
    🔹Ab "hello" print hone ke baad program ko stop hona chahiye 
         tha.
    🔹but kyunki maine break; ka use nhi kiya hain toh woh Default 
         tak print karega and then program stop hoga
    ➤ output : hello
                      hi
                      hi
                     Doesn't Matched

---
+++ Flow_Control/Flow_Control1.java
@@ -0,0 +1,18 @@
+package FLOW_CONTROL;
+
+public class FlowControl1 {
+    public static void main  (String args []){
+        byte var1 = 10;
+        boolean var2 = true;
+        switch (var1) {
+           case 10:
+              System.out.println("hello");
+           case 15:
+              System.out.println("hi");
+           case 20:
+              System.out.println("hi");
+           default:
+              System.out.println("Doesn't Matched");
+        }
+    }
+}
