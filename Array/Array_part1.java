bc20afd2a3603f46fb82535247f2d82ad6616f3c
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-21 16:26:19 +0530

    Which of the following are valid
    Only this one is not valid int[] []a,[]b;  Because If we want to specify the dimension before the variable that facility is available only for the first variable in the declaration.

--- Array/Array_part1.java
+++ Array/Array_part1.java
@@ -22,5 +22,18 @@ public class Array_Part1 {
         // int[] []x4;
         // int[]  x5[];
         // int  []x6[];
+
+        //?  Which of the following are valid */
+    
+     //  int[] x,y;
+    //  int[]  x1[],y1;
+    //  int[] x2[],y2[];
+//Space is ignored by the compiler 
+    //  int[] []x4,y4;
+    //  int[] []x5,y5[];
+
+//!  int[] []a,[]b; 
+
+        
     }
 }
