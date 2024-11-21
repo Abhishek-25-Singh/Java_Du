bde1fd122614770e5ac17a6f5c04b3c61d225b13
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-21 16:30:15 +0530

    Get Array Corresponding class

--- Array/Array_part1.java
+++ Array/Array_part1.java
@@ -60,6 +60,48 @@ public class Array_Part1 {
 // //! int[] x1 = new int[];
 
 // int[] x2 = new int[0];
+
+//******************************************************* */
+//** Get Array Corresponding class                        */
+//******************************************************* */
+
+// int[] a =new int[3];
+// System.out.println(a.getClass().getName());
+// int[][] a1 = new int[3][2];
+// System.out.println(a1.getClass().getName());
+// int[][][] a3 = new int[3][2][1];
+// System.out.println(a3.getClass().getName());
+
+
+// short[] s = new short[3];
+// System.out.println(s.getClass().getName());
+// short[][] s1 = new short[3][2];
+// System.out.println(s1.getClass().getName());
+// short[][][] s2 = new short[3][2][1];
+// System.out.println(s2.getClass().getName());
+
+// double[] d = new double[3];
+// System.out.println(d.getClass().getName());
+// double[][] d1 = new double[3][2];
+// System.out.println(d1.getClass().getName());
+// double[][][] d2 = new double[3][2][1];
+// System.out.println(d2.getClass().getName());
+
+
+// byte[] b = new byte[3];
+// System.out.println(b.getClass().getName());
+// byte[][] b1 = new byte[3][2];
+// System.out.println(b1.getClass().getName());
+// byte[][][] b2 = new byte[3][2][1];
+// System.out.println(b2.getClass().getName());
+
+
+// boolean[] bol = new boolean[3];
+// System.out.println(bol.getClass().getName());
+// boolean[][] bol1 = new boolean[3][2];
+// System.out.println(bol1.getClass().getName());
+// boolean[][][] bol2 = new boolean[3][2][1];
+// System.out.println(bol2.getClass().getName());
         
     }
 }
