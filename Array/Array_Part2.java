c953680acf6c65e127419268a23a3dad019fb19e
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-22 16:10:30 +0530

    Two Dimensional Array

--- Array/Array_Part2.java
+++ Array/Array_Part2.java
@@ -2,7 +2,14 @@ public class Array_part2
 {
     public static void main (String[] args){
 
+//******************************************************* */
+//** Two Dimensional Array Creation                                 */
+//******************************************************* */
 
+//? We have to at least Specify the base Size  
+// int[][] x = new int[2][];
+// x[0]= new int[2];
+// x[1]= new int[3];
 
 
     }
