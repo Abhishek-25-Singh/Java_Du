f8b9631d8d4e31fec9e79927d51308c60fb8c044
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-25 22:43:25 +0530

    Creation + Declaration + Initialization
    Used Three line but we can do this using one line .

--- Array/Array_part3.java
+++ Array/Array_part3.java
@@ -1,4 +1,12 @@
  class Array_part3 {
     public static void main (String[] args){
+             // //? Creation
+        // int[] x;
+        // //? Declaration
+        //  x = new int[3];
+        //   //initialization
+        // x[0]=1;
+        // x[1]=2;
+        // x[2]=3;
     }
  }
