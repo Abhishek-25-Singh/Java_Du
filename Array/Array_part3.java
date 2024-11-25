02d05fd3d9016cd87d8f3869d559515ecff6623b
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-25 22:45:29 +0530

    Array Declaration + Creation + Initialization in to single line
    🔹Ise program ke speed yeh internally kuch bhi fark nahi aayega 
         but yeh karne se programmer ko jyada type nahi karna hoga.
    🔹lines of code will reduce .

--- Array/Array_part3.java
+++ Array/Array_part3.java
@@ -8,5 +8,12 @@
         // x[0]=1;
         // x[1]=2;
         // x[2]=3;
+//******************************************************* */
+//** Array Declaration + Creation + Initialization in to single line                              */
+//******************************************************* */
+
+// int[] x1 = {1,2,3};
+// char[] ch = {'a','e','i','o','u'};
+// String[] s = {"abhishek","Mithilesh","Guddi","Anurag"};
     }
  }
