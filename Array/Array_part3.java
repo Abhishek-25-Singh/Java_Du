721c3f40a06fc9aef651c78c6d1e6893be9b2a61
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-29 21:29:38 +0530

    Two-Dimension And Three-Dimension
     Array Declaration + Creation + Initialization in to single line 
     Two-Dimension And Three-Dimension

--- Array/Array_part3.java
+++ Array/Array_part3.java
@@ -15,5 +15,22 @@
 // int[] x1 = {1,2,3};
 // char[] ch = {'a','e','i','o','u'};
 // String[] s = {"abhishek","Mithilesh","Guddi","Anurag"};
+
+//******************************************************************************************* */
+//** Array Declaration + Creation + Initialization in to single line 
+//?   Two-Dimension And Three-Dimension                                                                    */                            
+//******************************************************************************************************/  
+
+     // int[][] x = {{10,20},{30,40,50}};
+
+// int[][][] x1 ={{{10,20,30},{40,50,60}},{{70,80},{90,100,110}}};
+
+// System.out.println(x1[0][1][2]);
+// System.out.println(x1[1][0][1]);
+// //! System.out.println(x1[2][0][0]); -->  java.lang.ArrayIndexOutOfBoundsException: 2
+// //! System.out.println(x1[1][2][0]);   -->  java.lang.ArrayIndexOutOfBoundsException: 2
+// System.out.println(x1[1][1][1]);
+// //! System.out.println(x1[2][1][0]);   -->  java.lang.ArrayIndexOutOfBoundsException: 2
+     
     }
  }
